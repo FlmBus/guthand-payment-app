2c522c2525c1c159268ad3491ca79ab6d36681c2
Flemming Busman <flemming.busman@gmail.com>
2020-08-27 22:53:06 +0200

    Fix navbar alignment

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,13 +1,19 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container collapse navbar-collapse" id="navbarNav">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <ul class="navbar-nav my-2 my-lg-0">
-                <router-link class="navbar-brand" to="/">Payment App</router-link>
-            </ul>
-            <ul class="navbar-nav">
+    <nav class="navbar container navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Guthand Payment App</a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
                     <router-link class="nav-link" to="/dashboard">Übersicht</router-link>
                 </li>
@@ -21,7 +27,7 @@
                     <router-link class="nav-link" to="/withdraw">Auszahlung</router-link>
                 </li>
             </ul>
-            <ul class="navbar-nav my-2 my-lg-0">
+            <ul class="navbar-nav">
                 <li class="nav-item">
                     <a href="#" class="nav-link" @click.prevent="onLogout">Abmelden</a>
                 </li>
@@ -31,13 +37,13 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    name: 'Navigation',
+    name: "Navigation",
     methods: {
         async onLogout() {
-            const res = await axios.post('/logout');
+            const res = await axios.post("/logout");
         },
     },
 };
@@ -48,6 +54,7 @@ nav {
     margin-bottom: 3em;
 }
 nav ul.navbar-nav li.nav-item a.router-link-active {
-    color: rgba(0,0,0,.9);
+    color: #fff;
+    font-weight: bold;
 }
 </style>
